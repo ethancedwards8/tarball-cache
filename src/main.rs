52b14b28b1037b28bdbe205f98cdf127a2d38c51
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-07-30 21:41:25 -0700

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use axum::{
     routing::get,
     Router,
+    Json,
 };
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ErrResponse {
+    error: &'static str
+}
 
 #[tokio::main]
 async fn main() {
+    let app = Router::new()
+        .route("/", get(fallback))
+        .fallback(fallback);
 
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+   axum::serve(listener, app).await.unwrap();
+}
+
+async fn fallback() -> Json<ErrResponse> {
+    Json(ErrResponse{ error: "expected /github/{owner}/{repo}/{rev}.tar.gz"})
 }
