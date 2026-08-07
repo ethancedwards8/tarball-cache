dceae7808cf26215262384354290b752c9a30ca1
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-07 02:33:53 -0700

    sourcehut fix

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -I http://localhost:3000/github/nixos/nixpkgs/78ee0abaa454bc057b6e5623b188b9f4b87be24a.tar.gz
-curl -I http://localhost:3000/misterio/nix-colors/archive/81c0629d3a9a77e2a1d0b381a91760e34149a97d.tar.gz
+curl -I http://localhost:3000/sourcehut/misterio/nix-colors/81c0629d3a9a77e2a1d0b381a91760e34149a97d.tar.gz
 curl -I http://localhost:3000/github/nixos/nixpkgs/78ee0abaa454bc057b6e5623b188b9f4b87be24a.tar.gz
